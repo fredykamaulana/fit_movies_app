98c8b43e05fa1ab49b32ec5c3740150a5b3d2331
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-09 00:28:46 +0700

    create favourite movie hive object

---
+++ lib/data/db/favourite_movie.dart
@@ -0,0 +1,27 @@
+import 'package:hive/hive.dart';
+
+part 'favourite_movie.g.dart';
+
+@HiveType(typeId: 1)
+class FavouriteMovie {
+
+  @HiveField(0)
+  final int id;
+
+  @HiveField(1)
+  final String title;
+
+  @HiveField(2)
+  final String releaseDate;
+
+  //posterpath
+  @HiveField(3)
+  final String posterPath;
+
+  FavouriteMovie({
+    required this.id,
+    required this.title,
+    required this.releaseDate,
+    required this.posterPath,
+  });
+}
