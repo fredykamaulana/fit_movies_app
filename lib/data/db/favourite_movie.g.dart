9ec68622406a4887f48dee51db05fd0b138db34d
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-09 00:29:07 +0700

    generate favourite movie hive object adapter

---
+++ lib/data/db/favourite_movie.g.dart
@@ -0,0 +1,48 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'favourite_movie.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class FavouriteMovieAdapter extends TypeAdapter<FavouriteMovie> {
+  @override
+  final int typeId = 1;
+
+  @override
+  FavouriteMovie read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return FavouriteMovie(
+      id: fields[0] as int,
+      title: fields[1] as String,
+      releaseDate: fields[2] as String,
+      posterPath: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, FavouriteMovie obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.releaseDate);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FavouriteMovieAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
