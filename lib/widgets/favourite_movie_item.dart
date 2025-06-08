25c1a61eb30479513f4e81c172fbe9ef3c40b64e
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-09 00:29:34 +0700

    create fav movie item widget

---
+++ lib/widgets/favourite_movie_item.dart
@@ -0,0 +1,70 @@
+import 'package:fit_movies_app/data/db/favourite_movie.dart';
+import 'package:fit_movies_app/data/network/dio_api_client.dart';
+import 'package:fit_movies_app/navigations/navigation_routes.dart';
+import 'package:flutter/material.dart';
+
+class FavouriteMovieItem extends StatelessWidget {
+  final FavouriteMovie movie;
+
+  const FavouriteMovieItem({super.key, required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(
+            context,
+            NavigationRoutes.movieDetail.name,
+            arguments: movie.id
+        );
+      },
+      child: Card(
+        child: AspectRatio(
+          aspectRatio: 16/9,
+          child: Stack(
+            alignment: AlignmentDirectional.bottomStart,
+            fit: StackFit.expand,
+            clipBehavior: Clip.none,
+            children: [
+              ClipRRect(
+                  borderRadius: BorderRadius.circular(10.0),
+                  child: Hero(
+                      tag: movie.id,
+                      child: Image.network(
+                        '$imageUrl${movie.posterPath}',
+                        fit: BoxFit.fill,
+                        color: Colors.black.withValues(alpha: 100),
+                        colorBlendMode: BlendMode.darken,
+                      )
+                  )
+              ),
+              Positioned(
+                  bottom: 16,
+                  left: 16,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                        ),
+                      ),
+                      Text(
+                          movie.releaseDate,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(fontSize: 12, color: Colors.white70)
+                      ),
+                    ],
+                  )
+              )
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
