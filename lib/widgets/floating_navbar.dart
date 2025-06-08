1161ff5103fc0c40a198ddcdaac831015671ab8f
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-09 00:26:31 +0700

    create floating nav bar widget

---
+++ lib/widgets/floating_navbar.dart
@@ -0,0 +1,41 @@
+import 'package:fit_movies_app/controllers/movie_list_controller/movie_list_controller.dart';
+import 'package:fit_movies_app/widgets/floating_menu_navbar.dart';
+import 'package:fit_movies_app/widgets/floating_search_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class FloatingNavbar extends StatefulWidget {
+  const FloatingNavbar({super.key});
+
+  @override
+  State<FloatingNavbar> createState() => _FloatingNavbarState();
+}
+
+class _FloatingNavbarState extends State<FloatingNavbar> {
+
+  MovieListController movieListController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+      ),
+      elevation: 5,
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Obx(() {
+          bool isSearching = movieListController.isSearching;
+          return switch(isSearching) {
+            true => FloatingSearchBar(),
+            false => MainMenuNavbar(movieListController: movieListController),
+          };
+        }),
+      ),
+    );
+  }
+}
