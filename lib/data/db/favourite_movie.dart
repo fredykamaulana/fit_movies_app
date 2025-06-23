554de0a36bdb220ffce011ae0a2c2ba555359a3f
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-23 13:11:06 +0700

    create map function to and from json

--- lib/data/db/favourite_movie.dart
+++ lib/data/db/favourite_movie.dart
@@ -4,7 +4,6 @@ part 'favourite_movie.g.dart';
 
 @HiveType(typeId: 1)
 class FavouriteMovie {
-
   @HiveField(0)
   final int id;
 
@@ -23,4 +22,18 @@ class FavouriteMovie {
     required this.releaseDate,
     required this.posterPath,
   });
+
+  factory FavouriteMovie.fromJson(Map<String, dynamic> json) => FavouriteMovie(
+        id: json["id"],
+        title: json["title"],
+        releaseDate: json["release_date"],
+        posterPath: json["poster_path"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "release_date": releaseDate,
+        "poster_path": posterPath
+      };
 }
