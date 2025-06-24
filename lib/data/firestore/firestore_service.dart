dbe9895171d28b700578d74bab33887d4977fc32
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-24 11:39:07 +0700

    create firestore service class

---
+++ lib/data/firestore/firestore_service.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fit_movies_app/data/db/favourite_movie.dart';
+
+class FirestoreService {
+  final moviesRef = FirebaseFirestore.instance
+      .collection('favourite-movie')
+      .withConverter<FavouriteMovie>(
+        fromFirestore: (snapshots, _) =>
+            FavouriteMovie.fromJson(snapshots.data()!),
+        toFirestore: (movie, _) => movie.toJson(),
+      );
+
+  Future addFavouriteMovie(FavouriteMovie movie) async {
+    await moviesRef.doc(movie.id.toString()).set(movie);
+  }
+
+  Future removeFavouriteMovie(int movieId) async {
+    await moviesRef.doc(movieId.toString()).delete();
+  }
+
+  Future<bool> isFavourite(int movieId) async {
+    final result = await moviesRef.doc(movieId.toString()).get();
+    return result.exists;
+  }
+
+  Future<List<FavouriteMovie>> getAllFavouriteMovies() async {
+    final dataSnapshot = await moviesRef.get();
+
+    return dataSnapshot.docs.map((doc) => doc.data()).toList();
+  }
+}
