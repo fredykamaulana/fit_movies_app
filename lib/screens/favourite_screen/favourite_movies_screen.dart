39df06456f990cbb11c092d9d832be8128e0d283
fredykamaulana <fredyka.maulana@gmail.com>
2025-06-28 08:35:16 +0700

    add real time functionality

--- lib/screens/favourite_screen/favourite_movies_screen.dart
+++ lib/screens/favourite_screen/favourite_movies_screen.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fit_movies_app/controllers/favourite_movie_controller/favourite_movie_controller.dart';
 import 'package:fit_movies_app/controllers/movie_list_controller/movie_filter.dart';
 import 'package:fit_movies_app/controllers/movie_list_controller/movie_list_controller.dart';
+import 'package:fit_movies_app/data/db/favourite_movie.dart';
 import 'package:fit_movies_app/data/firestore/firestore_service.dart';
 import 'package:fit_movies_app/widgets/favourite_movie_item.dart';
 import 'package:flutter/material.dart';
@@ -64,5 +66,54 @@ class _FavouriteMovieScreenState extends State<FavouriteMovieScreen> {
               )
           };
         }));
+  }
+}
+
+class FavouriteMovieContent extends StatelessWidget {
+  final List<FavouriteMovie> favouriteMovies;
+  const FavouriteMovieContent({super.key, required this.favouriteMovies});
+
+  @override
+  Widget build(BuildContext) {
+    return Obx(() {
+      return switch (favouriteMovies.isNotEmpty) {
+        true => Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 8.0,
+                  crossAxisSpacing: 8.0,
+                  childAspectRatio: 0.7, // Adjust aspect ratio as needed
+                ),
+                itemCount: favouriteMovies.length,
+                itemBuilder: (context, index) {
+                  final movie = favouriteMovies[index];
+
+                  return FavouriteMovieItem(movie: movie);
+                })),
+        _ => const Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [Text("No favourite movies")]),
+          )
+      };
+    });
+  }
+}
+
+class StreamFavouriteMovie extends StatefulWidget {
+  final Stream<QuerySnapshot> snapshots;
+  const StreamFavouriteMovie({super.key, required this.snapshots});
+
+  @override
+  State<StreamFavouriteMovie> createState() => _StreamFavouriteMovieState();
+}
+
+class _StreamFavouriteMovieState extends State<StreamFavouriteMovie> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
