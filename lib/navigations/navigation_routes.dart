850e6a613c75f1ec8fc036a7d2fa8995124bfb18
fredykamaulana <fredyka.maulana@gmail.com>
2025-07-01 14:05:25 +0700

    add register and login screen

--- lib/navigations/navigation_routes.dart
+++ lib/navigations/navigation_routes.dart
@@ -1,8 +1,10 @@
 enum NavigationRoutes {
+  signin(name: '/signin'),
+  register(name: '/register'),
   movieList(name: '/movie/list'),
   movieDetail(name: '/movie/detail'),
   favourite(name: '/movie/favourite');
 
-  const NavigationRoutes({ required this.name });
+  const NavigationRoutes({required this.name});
   final String name;
 }
