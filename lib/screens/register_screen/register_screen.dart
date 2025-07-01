30bef3657fd5750be475aee4b77767f74dcb092d
fredykamaulana <fredyka.maulana@gmail.com>
2025-07-01 14:06:59 +0700

    create register screen

---
+++ lib/screens/register_screen/register_screen.dart
@@ -0,0 +1,127 @@
+import 'package:fit_movies_app/controllers/auth_controller/auth_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController psswdController = TextEditingController();
+
+  AuthController authController = Get.find();
+
+  bool passwordVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: const Text("Favourite Movies"),
+      ),
+      body: Center(
+          child: SizedBox(
+        width: 400,
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Text(
+                    'Daftar dah',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                ],
+              ),
+              SizedBox.square(dimension: 32),
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  hintText: 'Masukan Email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+              SizedBox.square(dimension: 16),
+              TextField(
+                controller: psswdController,
+                obscureText: passwordVisible,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Masukan Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    suffixIcon: IconButton(
+                      icon: Icon(passwordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off),
+                      onPressed: () {
+                        setState(() {
+                          passwordVisible = !passwordVisible;
+                        });
+                      },
+                    )),
+              ),
+              SizedBox.square(dimension: 16),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await authController.register(
+                        emailController.text, psswdController.text);
+
+                    if (mounted) {
+                      if (result != null) {
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content:
+                                Text('Sukses daftar sebagai ${result.email}')));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Masuk gagal')));
+                      }
+                    }
+                    emailController.clear();
+                    psswdController.clear();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                  ),
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text('Daftar'),
+                    ),
+                  )),
+              SizedBox.square(dimension: 32),
+              Row(
+                children: [
+                  Text('Sudah punya akun?'),
+                  SizedBox.square(dimension: 4),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Text('Masuk'))
+                ],
+              )
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+}
