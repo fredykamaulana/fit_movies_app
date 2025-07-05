d0c756ad9feb544a9d4090f63f365f3002184027
fredykamaulana <fredyka.maulana@gmail.com>
2025-07-05 22:42:38 +0700

    implement user management session

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:fit_movies_app/controllers/auth_controller/auth_controller.dart';
+import 'package:fit_movies_app/data/auth/firebase_auth_service.dart';
 import 'package:fit_movies_app/firebase_options.dart';
 import 'package:fit_movies_app/navigations/navigation_routes.dart';
 import 'package:fit_movies_app/screens/favourite_screen/favourite_movies_screen.dart';
@@ -9,6 +12,7 @@ import 'package:fit_movies_app/screens/register_screen/register_screen.dart';
 import 'package:fit_movies_app/screens/sign_in_screen/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get/get.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,40 +38,61 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  var initialRoute = NavigationRoutes.signin.name;
+  final AuthController authController =
+      Get.put(AuthController(Get.put(FirebaseAuthService())));
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Stay tune the movie!',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      themeMode: ThemeMode.system,
-      initialRoute: NavigationRoutes.signin.name,
-      routes: {
-        NavigationRoutes.signin.name: (context) => SignInScreen(),
-        NavigationRoutes.register.name: (context) => RegisterScreen(),
-        NavigationRoutes.movieList.name: (context) => MovieListScreen(),
-        NavigationRoutes.movieDetail.name: (context) => MovieDetailScreen(
-              movieId: ModalRoute.of(context)?.settings.arguments as int,
-            ),
-        NavigationRoutes.favourite.name: (context) => FavouriteMovieScreen(),
-      },
-    );
+    final signInState = authController.checkUserSignInState();
+
+    return StreamBuilder<User?>(
+        stream: signInState,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.active) {
+            return MaterialApp(
+              title: 'Stay tune the movie!',
+              theme: ThemeData(
+                // This is the theme of your application.
+                //
+                // TRY THIS: Try running your application with "flutter run". You'll see
+                // the application has a purple toolbar. Then, without quitting the app,
+                // try changing the seedColor in the colorScheme below to Colors.green
+                // and then invoke "hot reload" (save your changes or press the "hot
+                // reload" button in a Flutter-supported IDE, or press "r" if you used
+                // the command line to start the app).
+                //
+                // Notice that the counter didn't reset back to zero; the application
+                // state is not lost during the reload. To reset the state, use hot
+                // restart instead.
+                //
+                // This works for code too, not just values: Most code changes can be
+                // tested with just a hot reload.
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+                useMaterial3: true,
+              ),
+              themeMode: ThemeMode.system,
+              initialRoute: snapshot.data != null
+                  ? NavigationRoutes.movieList.name
+                  : NavigationRoutes.signin.name,
+              routes: {
+                NavigationRoutes.signin.name: (context) => SignInScreen(),
+                NavigationRoutes.register.name: (context) => RegisterScreen(),
+                NavigationRoutes.movieList.name: (context) => MovieListScreen(),
+                NavigationRoutes.movieDetail.name: (context) =>
+                    MovieDetailScreen(
+                      movieId:
+                          ModalRoute.of(context)?.settings.arguments as int,
+                    ),
+                NavigationRoutes.favourite.name: (context) =>
+                    FavouriteMovieScreen(),
+              },
+            );
+          }
+
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        });
   }
 }
