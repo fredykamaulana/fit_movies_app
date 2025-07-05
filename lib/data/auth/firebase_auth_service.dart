61dd72051f661c6a8dcfd3e0af76fe55bf75117b
fredykamaulana <fredyka.maulana@gmail.com>
2025-07-05 22:41:50 +0700

    refine auth service

--- lib/data/auth/firebase_auth_service.dart
+++ lib/data/auth/firebase_auth_service.dart
@@ -11,36 +11,16 @@ class FirebaseAuthService {
   final serverClientId =
       '179049673161-v1n7npj19qqvt7eanj09pa8p4tpfrajm.apps.googleusercontent.com';
 
-  Future<User?> registerWithEmailAndPassword(
+  Future<UserCredential> registerWithEmailAndPassword(
       String email, String password) async {
-    try {
-      final credential = await firebaseAuth.createUserWithEmailAndPassword(
-          email: email, password: password);
-
-      return credential.user;
-    } on FirebaseException catch (e) {
-      print(e);
-    } catch (e) {
-      print(e);
-    }
-
-    return null;
+    return await firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
   }
 
-  Future<User?> signInWithEmailAndPassword(
+  Future<UserCredential> signInWithEmailAndPassword(
       String email, String password) async {
-    try {
-      final credential = await firebaseAuth.signInWithEmailAndPassword(
-          email: email, password: password);
-
-      return credential.user;
-    } on FirebaseException catch (e) {
-      print(e);
-    } catch (e) {
-      print(e);
-    }
-
-    return null;
+    return await firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
   }
 
   Stream<User?> checkUserSignInState() {
@@ -48,36 +28,25 @@ class FirebaseAuthService {
     return state;
   }
 
-  Future<UserCredential?> signInWithGoogle() async {
-    try {
-      // Trigger the authentication flow
-      unawaited(googleSignIn.initialize(
-          clientId: clientId, serverClientId: serverClientId));
+  Future<UserCredential> signInWithGoogle() async {
+    unawaited(googleSignIn.initialize(
+        clientId: clientId, serverClientId: serverClientId));
 
-      final GoogleSignInAccount? googleUser = await googleSignIn.authenticate();
+    final GoogleSignInAccount googleUser = await googleSignIn.authenticate();
 
-      if (googleUser == null) {
-        // The user canceled the sign-in
-        return null;
-      }
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = googleUser.authentication;
 
-      // Obtain the auth details from the request
-      final GoogleSignInAuthentication googleAuth = googleUser.authentication;
+    // Create a new credential
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+      idToken: googleAuth.idToken,
+    );
 
-      // Create a new credential
-      final OAuthCredential credential = GoogleAuthProvider.credential(
-        idToken: googleAuth.idToken,
-      );
-
-      // Once signed in, return the UserCredential
-      return await FirebaseAuth.instance.signInWithCredential(credential);
-    } catch (e) {
-      print(e);
-      return null;
-    }
+    // Once signed in, return the UserCredential
+    return await firebaseAuth.signInWithCredential(credential);
   }
 
-  signOutWithGoogle() {
+  signOut() {
     googleSignIn.signOut();
     firebaseAuth.signOut();
   }
