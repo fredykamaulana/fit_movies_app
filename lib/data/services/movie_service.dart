0e9e25de516167964044664938a838978e8a9781
fredykamaulana <fredyka.maulana@gmail.com>
2025-07-09 22:03:43 +0700

    make every thing run normaly

--- lib/data/services/movie_service.dart
+++ lib/data/services/movie_service.dart
@@ -1,16 +1,43 @@
 import 'package:fit_movies_app/data/network/http_api_client.dart';
 import 'package:fit_movies_app/data/responses/movie_detail_response.dart';
 import 'package:fit_movies_app/data/responses/movie_list_response.dart';
+import 'package:flutter/foundation.dart';
 
 class MovieService {
   final serviceName = '/movie';
   final http = HttpApiClient();
 
   Future<MovieListResponse> fetchMovies(String filter) async {
-    return MovieListResponse(results: []);
+    try {
+      if (kDebugMode) {
+        print('$serviceName/$filter');
+      }
+
+      final response = await http.client.get(
+          Uri.parse('${http.baseUrl}/$serviceName/$filter'),
+          headers: http.headers);
+      if (response.statusCode == 200) {
+        return movieListResponseFromJson(response.body);
+      } else {
+        throw Exception('Failed to load movies');
+      }
+    } catch (e) {
+      throw Exception('Failed to load movies: ${e.toString()}');
+    }
   }
 
   Future<MovieDetailResponse> fetchMovieDetails(int movieId) async {
-    return MovieDetailResponse();
+    try {
+      final response = await http.client.get(
+          Uri.parse('${http.baseUrl}/$serviceName/$movieId'),
+          headers: http.headers);
+      if (response.statusCode == 200) {
+        return movieDetailResponseFromJson(response.body);
+      } else {
+        throw Exception('Failed to load movie details');
+      }
+    } catch (e) {
+      throw Exception('Failed to load movie details: $e');
+    }
   }
 }
--- lib/data/services/search_service.dart
+++ lib/data/services/search_service.dart
@@ -1,11 +1,28 @@
 import 'package:fit_movies_app/data/network/http_api_client.dart';
 import 'package:fit_movies_app/data/responses/movie_list_response.dart';
+import 'package:flutter/foundation.dart';
 
 class SearchService {
   final http = HttpApiClient();
   final serviceName = 'search/movie';
 
   Future<MovieListResponse> searchMovies(String query) async {
-    return MovieListResponse(results: []);
+    try {
+      if (kDebugMode) {
+        print('/search/movie?query=$query');
+      }
+
+      final response = await http.client.get(
+          Uri.parse('${http.baseUrl}/search/movie?query=$query'),
+          headers: http.headers);
+
+      if (response.statusCode == 200) {
+        return movieListResponseFromJson(response.body);
+      } else {
+        throw Exception('Failed to search movies');
+      }
+    } catch (e) {
+      throw Exception('Failed to search movies: $e');
+    }
   }
 }
