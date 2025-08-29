a5ee209ad93b27ff27bea3cbfb461a3544e5742d
Fredyka Maulana <fredyka.maulana@gmail.com>
2025-08-29 14:54:43 +0700

    complete test case of movie list robot

--- integration_test/robots/movie_list_screen_robot.dart
+++ integration_test/robots/movie_list_screen_robot.dart
@@ -7,10 +7,19 @@ class MovieListScreenRobot {
   MovieListScreenRobot({required this.tester});
 
   Future<void> tapSearchIcon() async {
-    final searchIconFinder = find.byKey(Key('search_icon_button'));
+    final mainMenuFinder = find.byKey(const Key('mainMenuNavbar'));
+    final searchIconFinder = find.byKey(const Key('searchIcon'));
 
+    // Scroll the main menu navbar to the end to make the search icon visible
+    await tester.drag(mainMenuFinder, const Offset(-500.0, 0.0));
+
+    // Ensure the search icon is visible before tapping
     await tester.ensureVisible(searchIconFinder);
+
+    // Tap the search icon
     await tester.tap(searchIconFinder);
+
+    // Wait for the UI to settle after the tap
     await tester.pumpAndSettle();
   }
 }
